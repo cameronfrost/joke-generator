0c922eb6f0b457d6d95b42f98d072f0de41c34ed
Cameron <c@cameronfrost.com>
2021-01-19 14:01:42 -0600

    Fetch joke API

---
+++ script.js
@@ -0,0 +1,17 @@
+const setupDiv = document.getElementById("setup");
+const punchlineDiv = document.getElementById("punchline");
+const punchlineBtn = document.getElementById("punchlineBtn");
+const newJokeBtn = document.getElementById("newJokeBtn");
+
+// fetch jokes from api
+async function getJoke() {
+  const jokePromise = await fetch(
+    "https://official-joke-api.appspot.com/jokes/programming/random"
+  );
+  const joke = await jokePromise.json();
+
+  // get the setup from the joke and insert it into the setupDiv element
+  setupDiv.innerHTML = joke[0].setup;
+}
+
+getJoke();
